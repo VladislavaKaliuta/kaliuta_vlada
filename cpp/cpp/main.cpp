7e7c722f43bfbdea6739cd927891a5ddae14fc58
VladislavaKaliuta <vlada18.02@mail.ru>
2021-09-27 00:16:54 +0300

    hello world
    console project

---
+++ cpp/cpp/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  cpp
+//
+//  Created by Vlada Vlada on 27.09.2021.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!"<< endl;
+    return 0;
+}
