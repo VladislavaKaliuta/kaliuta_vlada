f4cbf50bddff8d33109ee0656e41a707b10a6567
VladislavaKaliuta <vlada18.02@mail.ru>
2021-10-15 15:23:31 +0300

    checking errors/print menu

--- cpp/cpp/main.cpp
+++ cpp/cpp/main.cpp
@@ -10,9 +10,10 @@
 using namespace std;
 struct pipe
 {
-    int id;
-    double length;
-    double diameter;
+    int id = 0;
+    double length = 0;
+    double diameter = 0;
+    bool vremont = false;
 };
 struct station
 {
@@ -20,16 +21,27 @@ struct station
     string name;
     int tsekh;
     int worktsekh;
+    double effect;
 };
 
 pipe CreatePipe()
 {
     pipe p;
     p.id=0;
-    cout << "Enter length:";
-    cin >> p.length;
-    cout << "Enter diameter:";
-    cin >> p.diameter;
+    do
+    {
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << "Enter length:";
+        cin >> p.length;
+    }while (cin.fail() || p.length <= 0);
+    do
+    {
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << "Enter diameter:";
+        cin >> p.diameter;
+    }while (cin.fail() || p.diameter <= 0);
     return p;
 }
 station CreateStation()
@@ -38,10 +50,21 @@ station CreateStation()
     s.id=0;
     cout << "Enter name:";
     cin >> s.name;
-    cout << "Enter tsekh:";
-    cin >> s.tsekh;
-    cout << "Enter worktsekh:";
-    cin >> s.worktsekh;
+    do
+    {
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << "Enter tsekh (integer):";
+        cin >> s.tsekh;
+    }while (cin.fail() || s.tsekh <= 0);
+    do
+    {
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << "Enter worktsekh (integer):";
+        cin >> s.worktsekh;
+    }while (cin.fail() || s.worktsekh <= 0);
+    
     return s;
 }
 void PrintPipe(const pipe& p)
@@ -52,27 +75,123 @@ void PrintStation(const station& s)
 {
     cout << "Station name:" << s.name << "\ttsekh:" << s.tsekh << "\tworktsekh:" << s.worktsekh << endl;
 }
+void EditPipe(pipe& p)
+{
+    p.length-=1;
+}
 void SavePipeandStation(const pipe& p, const station& s)
 {
     ofstream fout;
-    fout.open("data.txt", 'w');
-    fout << "Pipe length:" << p.length << "\tdiameter:" << p.diameter << "Station name:" << s.name << "\ttsekh:" << s.tsekh << "\tworktsekh:" << s.worktsekh  << endl;
+    fout.open("data.txt", ios::out);
+    if (fout.is_open())
+    {
+        fout << p.length << endl << p.diameter << endl << s.name << endl << s.tsekh << endl << s.worktsekh  << endl;
+    fout.close();
+    }
+}
+pipe LoadPipe()
+{
+    pipe p;
+    ifstream fin;
+    fin.open("data.txt", ios::in);
+    if (fin.is_open())
+    {
+    fin >> p.length;
+    fin >> p.diameter;
+    fin.close();
+    }
+    return p;
+    
+}
+station LoadStation()
+{
+    station s;
+    ifstream fin;
+    fin.open("data.txt", ios::in);
+    if (fin.is_open())
+    {
+        fin >> s.name;
+    fin >> s.tsekh;
+    fin >> s.worktsekh;
+    fin.close();
+    }
+    return s;
+    
+}
+
+void Printmenu()
+{
+    cout << "(1) Enter Pipe" << endl
+    << "(2) Enter Station" << endl
+    << "(3) Print Pipe" << endl
+    << "(4) Print Station" << endl
+    << "(5) Save to file" << endl
+    << "(6) Load from file" << endl
+    << "(7) Edit Pipe" << endl
+    << "(0) Exit" << endl
+    << "Enter number, please:" << endl;
 }
 
 
-/*void EditPipe(pipe& p)
-{
-    p.length-=1;
-}*/
-
-
 int main() {
-    pipe p = CreatePipe();
-    station s = CreateStation();
-    PrintPipe(p);
-    PrintStation(s);
-   // EditPipe(p);
-    SavePipeandStation(p,s);
+    pipe p;
+    station s;
+    while (1)
+    {
+        Printmenu();
+        int i = 0;
+        cin >> i;
+        switch (i)
+        {
+            case 1:
+            {
+                CreatePipe();
+                break;
+            }
+            case 2:
+            {
+                CreateStation();
+                break;
+            }
+            case 3:
+            {
+                PrintPipe(p);
+                break;
+            }
+            case 4:
+            {
+                PrintStation(s);
+                break;
+            }
+            
+            case 5:
+            {
+                SavePipeandStation(p,s);
+                break;
+            }
+            case 6:
+            {
+                LoadPipe();
+                LoadStation();
+                break;
+            }
+            case 7:
+            {
+                EditPipe(p);
+                break;
+            }
+            case 0:
+            {
+               return 0;
+            }
+            default:
+                
+            {
+                cout << "wrong action" << endl;
+            }
+        }
+    }
+    
     return 0;
 }
 
